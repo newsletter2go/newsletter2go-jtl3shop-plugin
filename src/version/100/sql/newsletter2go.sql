48fa370cba705054fded16387897f90f5f2f48c3
Ahmad <ahmad@newsletter2go.com>
2020-11-16 18:59:41 +0100

    279492 | fix plugin updating issue (#14)
    * fix plugin updating issue
    
    * prepare new version 111 - 4.0.06

--- src/version/100/sql/newsletter2go.sql
+++ src/version/100/sql/newsletter2go.sql
@@ -1,2 +1,3 @@
-CREATE TABLE `xplugin_newsletter2go_keys` (`username` VARCHAR(100), `apikey` VARCHAR(100));
+CREATE TABLE IF NOT EXISTS `xplugin_newsletter2go_keys` (`username` VARCHAR(100), `apikey` VARCHAR(100));
+DELETE * FROM `xplugin_newsletter2go_keys`;
 INSERT INTO `xplugin_newsletter2go_keys` (`username`, `apikey`) VALUES ('nl2gosync', MD5(RAND()) );
